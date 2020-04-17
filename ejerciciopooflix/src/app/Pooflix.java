0a75a4bd37cec5d7ec712c344de99260d400f01a
Sophie-134 <sofiammendizabal@gmail.com>
2020-04-17 11:56:41 -0300

    Primer commit del ejercicio Pooflix

---
+++ ejerciciopooflix/src/app/Pooflix.java
@@ -0,0 +1,10 @@
+package app;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pooflix {
+    public List<Pelicula> peliculas = new ArrayList<>();
+    public List<Serie> series;
+
+}
